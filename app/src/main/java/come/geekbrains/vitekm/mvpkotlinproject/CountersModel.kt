833c276c1fecdb28011aed4cb47b6a3eb57afc26
ViktorMel <vitekm@ukr.net>
2022-08-03 12:17:31 +0300

    Lesson 1 add CountersModel

---
+++ app/src/main/java/come/geekbrains/vitekm/mvpkotlinproject/CountersModel.kt
@@ -0,0 +1,18 @@
+package come.geekbrains.vitekm.mvpkotlinproject
+
+class CountersModel {
+    private val counters = mutableListOf(0, 0, 0)
+
+    fun getCurrent(position: Int): Int {
+        return counters[position]
+    }
+
+    fun next(position: Int): Int {
+        return counters[position]++
+    }
+
+    fun set(position: Int, value: Int) {
+        counters[position] = value
+    }
+
+}
